fa7556e77bfbd5fb2687eb3a6923f57a8659ac12
monalsap27 <auliasaputri017@gmail.com>
2023-08-12 11:09:15 +0700

    vercel

--- src/elements/Header.jsx
+++ src/elements/Header.jsx
@@ -18,9 +18,9 @@ const Header = () => {
                     <p className="px-5 mt-12 md:w-3/4 md:mx-auto md:px-0">Tingkatkan skill dan pengetahuan bersama para mentor terbaik di bidangnya, untuk siapkan karir impian anda.</p>
                 </div>
                 <div className="relative w-full mt-8 md:right-0 md:top-44 md:absolute">
-                    <img src={Icon} alt="header image" className="absolute z-20 mt-2 md:-top-64 md:right-0"/>
-                    <img src={Icon1} alt="icon" className="absolute z-10 w-28 top-44 left-16 md:scale-y-150 md:left-2/4 md:ml-16 md:mt-1 md:top-40"/>
-                    <img src={Icon2} alt="icon" className="absolute z-10 w-28 top-12 md:-top-12 right-16 md:scale-150 md:right-52"/>
+                    <img src={Icon} alt="header image" className="absolute z-30 mt-5 md:-top-64 md:right-0"/>
+                    <img src={Icon1} alt="icon" className="absolute z-5 w-44 left-0.5 md:scale-y-150 md:left-2/4 md:ml-16 md:mt-1 md:top-44"/>
+                    <img src={Icon2} alt="icon" className="absolute z-10 w-44 top-12 md:-top-12 right-1 md:scale-150 md:right-60"/>
                 </div>
             </div>
         </div>
